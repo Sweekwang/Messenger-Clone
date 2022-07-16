2561f4c4f8780c1f57faa6e271de5f651256a34a
Swee Kwang <sweekwang94@gmail.com>
2022-07-16 19:18:04 +0800

    Initial Commit

---
+++ Messager/MessagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagerApp.swift
+//  Messager
+//
+//  Created by Swee Kwang Chua on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MessagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
